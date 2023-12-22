8f07521db3ac1b242530e94a9fa776546a2403fd
RohitM1518 <ROHIT.20211CSE0674@presidencyuniversity.in>
2023-12-22 17:42:14 +0530

    Refactor user controller(userLogin,userLogout) and add authentication middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,24 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {ApiError} from '../utils/ApiError.js'
-import {User} from '../models/user.model.js'
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from '../utils/ApiError.js'
+import { User } from '../models/user.model.js'
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
-const registerUser = asyncHandler(async(req,res) =>{
+const generateAccessAndRefreshToken=async(userId)=>{
+    try {
+        const user=await User.findById(userId);
+        const accessToken= user.generateAccessToken()
+        const refreshToken=user.generateRefreshToken()
+
+        user.refreshToken = refreshToken;
+        await user.save({validateBeforeSave:false})
+        return {accessToken,refreshToken}
+    } catch (error) {
+        throw new ApiError(500,"Something went wrong while generating tokens")
+    }
+}
+
+const registerUser = asyncHandler(async (req, res) => {
     // get user details from frontend
     // validation — not empty
     // check if user already exists: username, email
@@ -16,66 +30,129 @@ const registerUser = asyncHandler(async(req,res) =>{
     // check for user creation
     // return res
 
-    const {username,fullname,email,password}=req.body
-    console.log(username,fullname)
+    const { username, fullname, email, password } = req.body
+    console.log(username, fullname)
 
     // if(fullname==""){
     //     throw ApiError(400,"fullname is required")
     // }
 
-    if ([fullname, email, username, password].some((feild)=>feild?.trim()==="")){
-        throw ApiError(400,"All fields are required")
+    if ([fullname, email, username, password].some((feild) => feild?.trim() === "")) {
+        throw ApiError(400, "All fields are required")
     }
 
-    const existedUser=await User.findOne({
-        $or:[{ username },{ email }]
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
     })
-    if(existedUser){
-        throw new ApiError(409,"User with email or username already exists")
+    if (existedUser) {
+        throw new ApiError(409, "User with email or username already exists")
     }
     console.log(req)
-    const avatarLocalPath=req.files?.avatar[0]?.path;
+    const avatarLocalPath = req.files?.avatar[0]?.path;
     //const coverImageLocalPath=req.files?.coverImage[0]?.path;
 
     let coverImageLocalPath;
-    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >0) {
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path;
     }
 
-    if(!avatarLocalPath){
-        throw new ApiError(400,"Avatar is required")
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar is required")
     }
 
-    const avatar=await uploadOnCloudinary(avatarLocalPath)
-    const coverImage=await uploadOnCloudinary(coverImageLocalPath)
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-    if(!avatar){
-        throw new ApiError(400,"Avatar is required")
+    if (!avatar) {
+        throw new ApiError(400, "Avatar is required")
     }
 
     const user = await User.create(
         {
-        fullname,
-        avatar:avatar.url,
-        coverImage: coverImage?.url || "",
-        email,
-        password,
-        username:username?.toLowerCase()
-    }
+            fullname,
+            avatar: avatar.url,
+            coverImage: coverImage?.url || "",
+            email,
+            password,
+            username: username?.toLowerCase()
+        }
     )
 
-    const createdUser=await User.findById(user._id).select(
+    const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
-    
-    if(!createdUser){
-        throw new ApiError(500,"Something went wrong while registering")
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering")
     }
 
     return res.status(201).json(
-        new ApiResponse(200,createdUser)
+        new ApiResponse(200, createdUser)
     )
 })
 
+const loginUser = asyncHandler(async (req, res) => {
+    // req body —> data
+    // username or email
+    // find the user
+    //password check
+    //access and referesh token
+    // send cookie
 
-export {registerUser}
+const {email,username,password}=req.body
+if(!username && !email){
+    throw new ApiError(400,"Email or Username is required")
+}
+const user=await User.findOne({
+    $or:[{email},{username}]  //$or is the mongodb operator
+})
+
+if(!user){
+    throw new ApiError(404,"User Does not Exists")
+}
+
+const isPasswordValid=await user.isPasswordCorrect(password)
+if(!isPasswordValid){
+    throw new ApiError(401,"Invalid user Credentials")
+}
+
+const {accessToken,refreshToken}=await generateAccessAndRefreshToken(user._id)
+
+const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+const options={
+    httpOnly:true,
+    secure:true
+}
+
+return res
+.status(200)
+.cookie("accessToken",accessToken,options)
+.cookie("refreshToken",refreshToken,options)
+.json(new ApiResponse(
+    200,
+    {user:loggedInUser,accessToken,refreshToken},
+    "User Logged In Successfully"
+))
+
+})
+
+const logoutUser = asyncHandler(async(req,res)=>{
+    await User.findByIdAndUpdate(req.user._id,
+        {
+            $set:{
+                refreshToken:undefined
+            },
+           
+        },
+        {
+            new:true
+        })
+        const options={
+            httpOnly:true,
+            secure:true
+        }
+
+        return res.status(200).clearCookie("accessToken",options).clearCookie("refreshToken",options).json(new ApiResponse(200,{},"User logged Out"))
+})
+export { registerUser, loginUser,logoutUser }
