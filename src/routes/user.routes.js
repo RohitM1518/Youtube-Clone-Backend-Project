69beb2fb1698b4d420f6b2763a7007b79ef140a4
RohitM1518 <ROHIT.20211CSE0674@presidencyuniversity.in>
2024-02-09 18:43:21 +0530

    Update user.routes.js with new controller methods and routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,19 +1,19 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
-import {upload} from '../middlewares/multer.middleware.js'
+import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
+import { upload } from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
 router.route("/register").post(
     upload.fields([ //fields is used to upload multiple files
-    //Here we are using multer middleware to upload the file
-    //This will upload the file before the registerUser method is called
+        //Here we are using multer middleware to upload the file
+        //This will upload the file before the registerUser method is called
         {
-            name:"avatar",
-            maxCount:1
+            name: "avatar",
+            maxCount: 1
         },
         {
-            name:"coverImage",
-            maxCount:1
+            name: "coverImage",
+            maxCount: 1
         }
     ]),
     registerUser)
@@ -21,6 +21,15 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 
 //secured routes
-router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").post(verifyJWT, getCurrentUser)
+router.route("/update-details").patch(verifyJWT, updateAccountDetails) //patch is used to update only some feilds like name and id not all, if we use post it will update everything
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/update-coverimage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watchhistory").get(verifyJWT, getWatchHistory)
+
 export default router
