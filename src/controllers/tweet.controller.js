207b1cacb7a3a48d049920bd362c2ba14e7d58de
RohitM1518 <ROHIT.20211CSE0674@presidencyuniversity.in>
2024-02-11 16:14:38 +0530

    Add subscription and tweet controllers

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -2,8 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiError } from '../utils/ApiError.js'
 import { Tweet } from '../models/tweet.model.js'
 import { ApiResponse } from "../utils/ApiResponse.js"
-import jwt from "jsonwebtoken"
-import mongoose from "mongoose"
+
 
 const createTweet = asyncHandler(async (req, res) => {
     const {content} = req.body
